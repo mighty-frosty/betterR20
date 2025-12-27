44fac423fc5733e003f0090f6e16910b9ee316cc
helenclarko <helenclarko@gmail.com>
2025-12-26 22:16:19 -0500

    Added Image tokens menu back and the token ID copy

--- js/base-menu.js
+++ js/base-menu.js
@@ -315,6 +315,49 @@ function baseMenu () {
 			}
 		}
 
+		// Function to add Edit Token Images button
+		function addEditTokenImagesToMenu() {
+			const contextMenu = document.querySelector('.context-menu');
+			if (!contextMenu) return;
+
+			// Check if already added
+			if (contextMenu.querySelector('.edit-token-images-button')) return;
+
+			if (!window.is_gm) return;
+
+			// Create Edit Token Images button
+			const editBtn = document.createElement('button');
+			editBtn.className = 'edit-token-images-button';
+			editBtn.type = 'button';
+			editBtn.setAttribute('data-v-2aed8a8e', '');
+			editBtn.setAttribute('data-v-060adf8b', '');
+
+			editBtn.innerHTML = `
+				<div data-v-2aed8a8e="" class="submenu-button-outer">
+					<div data-v-2aed8a8e="" class="submenu-button-inner">
+						<div data-v-2aed8a8e="" style="display: flex; align-items: center; gap: 4px;">
+							<div data-v-2aed8a8e="" style="display: flex; align-items: center; gap: 8px;">
+								<span data-v-2aed8a8e="" style="flex: 1 1 0%; text-align: start;">Edit Token Images</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			`;
+
+			editBtn.addEventListener('click', () => {
+				d20plus.menu.editToken();
+				closeContextMenu();
+			});
+
+			// Insert after Copy Token ID button
+			const copyBtn = contextMenu.querySelector('.copy-tokenid-button');
+			if (copyBtn) {
+				copyBtn.parentNode.insertBefore(editBtn, copyBtn.nextSibling);
+			} else {
+				contextMenu.insertBefore(editBtn, contextMenu.firstChild);
+			}
+		}
+
 		// Watch for context menu appearing
 		const observer = new MutationObserver(() => {
 			const menu = document.querySelector('.context-menu');
@@ -331,6 +374,7 @@ function baseMenu () {
 					addSetLightToMenu();
 					addFlightHeightToMenu();
 					addCopyTokenIdToMenu();
+					addEditTokenImagesToMenu();
 				} else {
 					// Remove custom menu items from non-token menus
 					const existingMassRoll = menu.querySelector('.mass-roll-button');
@@ -348,6 +392,10 @@ function baseMenu () {
 					const existingCopy = menu.querySelector('.copy-tokenid-button');
 					if (existingCopy) {
 						existingCopy.remove();
+					}
+					const existingEdit = menu.querySelector('.edit-token-images-button');
+					if (existingEdit) {
+						existingEdit.remove();
 					}
 				}
 			}
@@ -609,6 +657,295 @@ function baseMenu () {
 
 		// Show prompt with token ID for copying
 		window.prompt("Copy to clipboard: Ctrl+C, Enter", tokenId);
+	};
+
+	// Edit Token Images - Constants and helpers
+	const tagSize = "#roll20_token_size=";
+	const tagSkip = "#roll20_skip_token=";
+
+	function tokenEditorTexts (selection) {
+		const name = selection.length > 1 ? "You are editing multiple tokens" : selection[0]._model?.attributes?.name || "Unnamed token";
+		const description = selection.length > 1 ? `
+			If you press "Save", the changes will be applied to each of the selected tokens, making them multi-sided if you have multiple images on the list below
+		` : selection[0]._model.attributes.sides ? `
+			You are currently editing images for multi-sided token. Add or remove as many sides as you want. If only one image remains, the token will become a single-sided one
+		` : `
+			Currently this token is represented by a single image. Add more images to convert it to multi-sided token
+		`;
+		const tokenList = selection.length <= 1 ? "" : selection.reduce((r, t) => `${r}
+			<div class="tokenbox selected" data-tokenid="${t._model.id}" data-tokenimg="${t._model.attributes.imgsrc}">
+				<div class="inner">
+					<img src="${t._model.attributes.imgsrc}">
+					<div class="name">${t._model.attributes.name}</div>
+				</div>
+			</div>
+		`, "");
+		return {name, description, tokenList};
+	}
+
+	// Edit Token Images function (adapted for Jumpgate)
+	d20plus.menu.editToken = (tokenId) => {
+		const selection = tokenId
+			? [{_model: d20plus.ut.getTokenById(tokenId)}].filter(t => !!t._model?.attributes)
+			: d20.engine.selected();
+		if (!selection.length) return;
+
+		const images = [];
+		const added = [];
+		const $dialog = $(d20plus.html.tokenImageEditor);
+		const $list = $dialog.find(".tokenimagelist tbody");
+		const $tokenList = $dialog.find(".tokenlist");
+		const sizes = [["tiny - half square", "0.5"], ["small - 1x1", "1.0"], ["medium - 1x1", "1"], ["large - 2x2", "2"], ["huge - 3x3", "3"], ["gargantuan - 4x4", "4"], ["colossal - 5x5", "5"], ["custom", "0"]];
+
+		const findStandardSize = (w, h) => {
+			return (w === h && sizes.find(s => s[1] === `${w / 70}`)?.last()) || "0";
+		};
+
+		const addImageOnInit = (img, add) => {
+			const sizeChanged = img.w !== images.last()?.w || img.h !== images.last()?.h;
+			if (images.length && sizeChanged) $list.variedSizes = true;
+			images.push(img);
+			added.push(add || img.url);
+		};
+
+		selection.forEach(t => {
+			const sides = t._model.attributes.sides?.split("|");
+			const token = t._model.attributes.imgsrc;
+			const {width: tw, height: th} = t._model.attributes;
+
+			if (sides && sides.length > 1) {
+				const curSide = sides[t._model.attributes.currentSide] || token;
+				sides.forEach((s, k) => {
+					const checked = unescape(s);
+					const listed = added.indexOf(checked);
+					const [url, size] = checked.split(tagSize);
+					const [sw, sh] = (size || "").split("x");
+					const image = {
+						url: url.replaceAll(tagSkip, ""),
+						skip: url.includes(tagSkip),
+						face: unescape(curSide).includes(url),
+						w: tw,
+						h: th,
+					};
+					if (listed !== -1) {
+						if (k === t._model.attributes.currentSide) images[listed].face = true;
+						return;
+					} else if (!isNaN(size)) {
+						Object.merge(image, {size, w: size * 70, h: size * 70});
+					} else if (!isNaN(sw) && !isNaN(sh)) {
+						Object.merge(image, {size: "0", w: sw, h: sh});
+					}
+					addImageOnInit(image, checked);
+				});
+			} else {
+				const listed = added.indexOf(t._model.attributes.imgsrc);
+				if (listed !== -1) images[listed].face = true;
+				else addImageOnInit({url: t._model.attributes.imgsrc, face: true, w: tw, h: th});
+			}
+		});
+
+		if ($list.variedSizes) {
+			images.forEach(i => { if (i.size === undefined) i.size = findStandardSize(i.w, i.h); });
+		}
+
+		const htmls = tokenEditorTexts(selection);
+
+		const resetTokens = () => {
+			$tokenList.find(".selected").each((k, t) => {
+				const $token = $(t);
+				const $tokenimage = $token.find("img");
+				$tokenimage.attr("src", $token.data("tokenimg"));
+			});
+		};
+
+		const buildList = () => {
+			if (images.length === 1) {
+				$list.someImageSelected = true;
+				images[0].selected = true;
+			}
+			$list.html(images.reduce((r, i, k) => `${r}
+				<tr class="tokenimage${images.length === 1 ? " lastone" : ""}${i.skip ? " skipped" : ""}" data-index="${(i.id = k, k)}">
+					<td style="padding:0px;" title="Current image">
+						<input class="face" type="checkbox"${i.selected ? " checked" : ""}>
+					</td>
+					<td>
+						<div class="dropbox filled">
+						<div class="inner"><img src="${i.url}"><div class="remove"><span>Drop a file</span></div></div>
+						</div>
+					</td>
+					<td>
+						<label>Select size:</label><select>${sizes.reduce((o, s) => `${o}
+							<option value="${s[1]}"${s[1] === i.size ? " selected" : ""}>${s[0]}</option>
+						`, `<option>default (keep as is)</option>`)}</select>
+						<span class="custom${i.size === "0" ? " set" : ""}"><input class="w" value="${i.w}"> X <input class="h" value="${i.h}">px</span>
+						<label class="skippable"><input class="toskip" type="checkbox"${i.skip ? " checked" : ""}> Skip side on randomize</label>
+					</td>
+					<td style="padding:0px;">
+						<span class="btn url" title="Edit URL...">j</span>
+						<span class="btn delete" title="Delete">#</span>
+					</td>
+				</tr>
+			`, ""));
+			if (!$list.someImageSelected) {
+				images.forEach((i, k) => {
+					if (i.face) $list.find("input.face").eq(k).prop({indeterminate: true});
+				});
+			}
+		};
+
+		$dialog.dialog({
+			autoopen: true,
+			title: "Edit token image(s)",
+			width: 450,
+			open: () => {
+				buildList();
+				$tokenList.html(htmls.tokenList);
+				$dialog.parent().css("maxHeight", "80vh").css("top", "10vh");
+				$dialog.find(".edittitle").text(htmls.name);
+				$dialog.find(".editlabel").text(htmls.description);
+
+				// Event handlers
+				$dialog.on("change", "select", evt => {
+					const $changed = $(evt.target);
+					const $token = $changed.parent();
+					const $custom = $token.find(".custom").removeClass("set");
+					const newSize = $changed.val();
+					const id = $changed.closest(".tokenimage").data("index");
+					if (newSize > 0) {
+						$token.find(".w, .h").val(newSize * 70);
+						images[id].size = newSize;
+						$list.variedSizes = true;
+					} else {
+						delete images[id].size;
+						if (newSize === "0") {
+							$list.variedSizes = true;
+							images[id].size = newSize;
+							images[id].w = $token.find(".w").val();
+							images[id].h = $token.find(".h").val();
+							$custom.addClass("set");
+						}
+					}
+				}).on("change", "input.face", evt => {
+					const id = $(evt.target).closest(".tokenimage").data("index");
+					const isChecked = $(evt.target).prop("checked");
+					const $allBoxes = $list.find("input.face");
+					if (isChecked) {
+						$list.someImageSelected = true;
+						$allBoxes.prop({checked: false}).prop({indeterminate: false});
+						$(evt.target).prop({checked: true});
+						$tokenList.find(".selected img").attr("src", images[id].url);
+						images.forEach((i, k) => {
+							if (k === id) i.selected = true;
+							else i.selected = false;
+						});
+					} else {
+						$list.someImageSelected = false;
+						images[id].selected = false;
+						resetTokens();
+						images.forEach((i, k) => {
+							if (i.face) $allBoxes.eq(k).prop({indeterminate: true});
+						});
+					}
+				}).on("change", "input.toskip", evt => {
+					const $token = $(evt.target).closest(".tokenimage");
+					const id = $token.data("index");
+					const isChecked = $(evt.target).prop("checked");
+					if (isChecked) {
+						$token.addClass("skipped");
+						images[id].skip = true;
+					} else {
+						$token.removeClass("skipped");
+						images[id].skip = false;
+					}
+				}).on(window.mousedowntype, ".url", evt => {
+					const $token = $(evt.target).closest(".tokenimage");
+					const $image = $token.find("img");
+					const id = $token.data("index");
+					const url = window.prompt("Edit URL", $image.attr("src"));
+					if (!url) return;
+					d20plus.art.setLastImageUrl(url);
+					images[id].url = url;
+					$image.attr("src", url);
+				}).on(window.mousedowntype, ".delete", evt => {
+					const $deleted = $(evt.target).closest(".tokenimage");
+					const id = $deleted.data("index");
+					if (images.length <= 1) return;
+					if (images[id].selected) {
+						$list.someImageSelected = false;
+						resetTokens();
+					}
+					images.splice(id, 1);
+					buildList();
+					if (images.length === 1) {
+						$list.someImageSelected = true;
+						$list.find("input.face").prop({checked: true});
+						$tokenList.find(".selected img").attr("src", images[0].url);
+					}
+				}).on(window.mousedowntype, ".addimageurl", () => {
+					const url = window.prompt("Enter a URL", d20plus.art.getLastImageUrl());
+					if (!url) return;
+					d20plus.art.setLastImageUrl(url);
+					images.push({url, w: 70, h: 70});
+					buildList();
+				});
+			},
+			close: () => {
+				$dialog.off();
+				$dialog.dialog("destroy").remove();
+			},
+			buttons: {
+				save: {
+					text: "Save changes",
+					click: () => {
+						const save = {};
+						if (images.length > 1) {
+							save.sides = images.map(i => {
+								const skipped = i.skip ? tagSkip : "";
+								const size = i.size ? tagSize + (i.size === "0" ? `${i.w}x${i.h}` : i.size) : "";
+								return escape(i.url + skipped + size);
+							}).join("|");
+						} else {
+							save.sides = "";
+						}
+						if ($list.someImageSelected) {
+							const selected = images.find(i => i.selected);
+							if (selected) {
+								save.imgsrc = selected.url;
+								save.currentSide = selected.id;
+								if (selected.size === "0") {
+									save.width = Number(selected.w);
+									save.height = Number(selected.h);
+								} else if (selected.size) {
+									save.width = selected.size * 70;
+									save.height = selected.size * 70;
+								}
+							}
+						}
+						if (selection.length > 1) {
+							d20.engine.unselect();
+						}
+						selection.forEach(t => {
+							if (selection.length === 1
+								|| $tokenList.find(`[data-tokenid=${t._model.id}]`).hasClass("selected")) {
+								t._model.save(save);
+							}
+						});
+						$dialog.off();
+						$dialog.dialog("destroy").remove();
+						d20.textchat.$textarea.focus();
+					},
+				},
+				cancel: {
+					text: "Cancel",
+					click: () => {
+						$dialog.off();
+						$dialog.dialog("destroy").remove();
+					},
+				},
+			},
+		});
+
+		return $dialog;
 	};
 
 	// Set Flight Height
