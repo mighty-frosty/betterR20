ee939e855a36c5ac263be0af84b21fd43f97e0ca
mighty-frosty <mighty-frosty@users.noreply.github.com>
2026-06-17 13:56:25 +0000

    deploy: testing build 1.36.1.3-beta-testing 0c73788a6e98e496e91cd553c0ee0a82a9a17b4f

---
+++ betteR20-5etools.meta.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         betteR20-beta-5etools-death-jumpagate-import
+// @namespace    https://5e.tools/
+// @license      MIT (https://opensource.org/licenses/MIT)
+// @version      1.36.1.3-beta-testing
+// @updateURL    https://mighty-frosty.github.io/betterR20/betteR20-5etools.meta.js
+// @downloadURL  https://mighty-frosty.github.io/betterR20/betteR20-5etools.user.js
+// @description  Enhance your Roll20 experience
+// @author       5egmegaanon/astranauta/MrLabRat/TheGiddyLimit/DBAWiseMan/BDeveau/Remuz/Callador Julaan/Erogroth/Stormy/FlayedOne/Cucucc/Cee/oldewyrm/darthbeep/Mertang/Redweller/DeathStalker
+
+// @match        https://app.roll20.net/editor
+// @match        https://app.roll20.net/editor#*
+// @match        https://app.roll20.net/editor?*
+// @match        https://app.roll20.net/editor/
+// @match        https://app.roll20.net/editor/#*
+// @match        https://app.roll20.net/editor/?*
+
+// @grant        unsafeWindow
+// @run-at       document-start
+
+// @grant        GM_webRequest
+// @webRequest   [{"selector": { "include": "*://www.google-analytics.com/analytics.js" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://cdn.userleap.com/shim.js?*" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://analytics.tiktok.com/*" },  "action": "cancel"}]
+
+// ==/UserScript==
---
+++ betteR20-core.meta.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         betteR20-beta-core-death-jumpagate-import
+// @namespace    https://5e.tools/
+// @license      MIT (https://opensource.org/licenses/MIT)
+// @version      1.36.1.3-beta-testing
+// @updateURL    https://mighty-frosty.github.io/betterR20/betteR20-core.meta.js
+// @downloadURL  https://mighty-frosty.github.io/betterR20/betteR20-core.user.js
+// @description  Enhance your Roll20 experience
+// @author       TheGiddyLimit/Redweller
+
+// @match        https://app.roll20.net/editor
+// @match        https://app.roll20.net/editor#*
+// @match        https://app.roll20.net/editor?*
+// @match        https://app.roll20.net/editor/
+// @match        https://app.roll20.net/editor/#*
+// @match        https://app.roll20.net/editor/?*
+
+// @grant        unsafeWindow
+// @run-at       document-start
+
+// @grant        GM_webRequest
+// @webRequest   [{"selector": { "include": "*://www.google-analytics.com/analytics.js" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://cdn.userleap.com/shim.js?*" },  "action": "cancel"}]
+// @webRequest   [{"selector": { "include": "*://analytics.tiktok.com/*" },  "action": "cancel"}]
+
+// ==/UserScript==
